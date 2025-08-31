deac7615e115ffb7f8a669222d297b0e5f599123
Dima <aligator451111@gmail.com>
2025-08-31 20:06:54 +0300

    Базова функціональність калькулятора з чотирма операціями

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Simple Calculator ===");
+            Console.WriteLine("Enter first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter operation (+, -, *, /):");
+            string operation = Console.ReadLine();
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
